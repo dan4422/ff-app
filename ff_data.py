08d9595910f97c642ab59807919df0c421bb881c
Dan4422 <sdlee095@gmail.com>
2022-12-01 18:41:10 -0500

    webscrape data from espn into object format

--- ff_data.py
+++ ff_data.py
@@ -3,38 +3,38 @@ from bs4 import BeautifulSoup
 ## how am i getting the url for each game every week? selenium may be able to help
 ## or find a new website that may be easier to parse and extract data from for each game. try this with
 ## Pro-football-reference.com
-html_text = requests.get('https://www.espn.com/nfl/boxscore/_/gameId/401437857').text
 
-soup = BeautifulSoup(html_text, 'html.parser')
-# write function to pass in each gampackage. ex. gamepackage-rushing, gamepackage-receiving, etc.
-# data = soup.find('div', id="gamepackage-passing")
-# home = data.find('div', class_='gamepackage-home-wrap')
-# away = data.find('div', class_='gamepackage-away-wrap')
-# print(home.find('td', class_='name').find('span').text)
-# print(home.find('td', class_='c-att').text)
-# print(home.find('td', class_='yds').text)
-# print(home.find('td', class_='td').text)
-# print(away.find('td', class_='name').find('span').text)
-# print(away.find('td', class_='c-att').text)
-# print(away.find('td', class_='yds').text)
-# print(away.find('td', class_='td').text)
+def request_html(url):
+  html_text = requests.get(url).text
+  soup = BeautifulSoup(html_text, 'html.parser')
+  return soup
 
-data = soup.find('div', id="gamepackage-rushing")
-home = data.find('div', class_='gamepackage-home-wrap')
-away = data.find('div', class_='gamepackage-away-wrap')
-names = home.find_all('td', class_='name')
-for n in names:
-  if (n.find('span') == None):
-    pass
-  else:
-    print(n.find('span').text)
-carries = home.find_all('td', class_='car')
-for c in carries:
-  print(c.text)
-# print(home.find('td', class_='yds').text)
-# print(home.find('td', class_='td').text)
-# print(away.find('td', class_='name').find('span').text)
-# print(away.find('td', class_='car').text)
-# print(away.find('td', class_='yds').text)
-# print(away.find('td', class_='td').text)
+def get_data(soup ,data_id, home_or_away):
+  return_data = {}
+  html = soup.find('div', id=data_id)
+  data = html.find('div', class_=f"gamepackage-{home_or_away}-wrap")
+  th = data.find_all('th', class_=True)
+  classes = [value
+           for element in th
+           for value in element["class"]]
+  names = data.find_all('td', class_=classes[0])
+  for c in classes[1:]:
+    stat_data = data.find_all('td', class_= c)
+    for n,d in zip(names,stat_data):
+      if (n.find('span') == None):
+        pass
+      else:
+        if (n.find('span').text in return_data):
+          obj = {c:d.text}
+          return_data[n.find('span').text] = {**return_data[n.find('span').text], **obj}
+        else:
+          return_data[n.find('span').text] = {c:d.text}
+  return return_data
+
+
+
+print(get_data(request_html('https://www.espn.com/nfl/boxscore/_/gameId/401437857'), 'gamepackage-passing', 'home'))
+print(get_data(request_html('https://www.espn.com/nfl/boxscore/_/gameId/401437857'), 'gamepackage-rushing', 'home'))
+print(get_data(request_html('https://www.espn.com/nfl/boxscore/_/gameId/401437857'), 'gamepackage-receiving', 'home'))
+print(get_data(request_html('https://www.espn.com/nfl/boxscore/_/gameId/401437857'), 'gamepackage-fumbles', 'home'))
 
